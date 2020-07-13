1165a9adf36c6cdd0f623731295b1dacfaa9b005
MaksShadows <Lykanenko1995@gmail.com>
2020-07-13 22:40:47 +0300

    add

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 
 
 class Event extends Component {
-    state={
-        events:[
-            {
-              id: 1,
-              title: 'One',
-              date: '08-07-2020',
-              startTime: '20:00',
-              endTime: '20:30',
-              description: 'first',
-          }, ]
-    }
+    // state={
+    //     events:[
+    //         {
+    //           id: 1,
+    //           title: 'One',
+    //           date: '08-07-2020',
+    //           startTime: '20:00',
+    //           endTime: '20:30',
+    //           description: 'first',
+    //       }, ]
+    // }
     render() {
 
         return (
 
             <div key={Math.random()}
              className="active_event">
-             {this.state.events.map((event) =>
+             {this.props.events.map((event) =>
                 `${event.title}
                  ${event.date}
                  ${event.startTime}-${event.endTime}
--- src/components/main/Day.jsx
+++ src/components/main/Day.jsx
@@ -3,35 +3,42 @@ import Event from "../Event";
 import  generateNumbers from "../common/utilities";
 
 
-const ColumnTable = ({column, events,}) => {
 
+const ColumnTable = ({column, events, id}) => {
 
   const columnDay = generateNumbers(0, 6).map(arg => {
 
+
     return (
-      <div   key={arg}   className="column-day">
-         
-        <>
-          {column = generateNumbers(0, 23).map(item =>{ 
-              return (
-                <div key={item}   className="column-item">
-                   <Event
-                    key={Math.random()}
-                    events={events}
-                  /> 
-                      {/* {events.map(event => id ===(event.date) &&(
-                       <Event
-                       key={event.id}
-                       id={event.id}
-                     />
-                     )
-                      )}  */}
-                </div>
-              );
-           }
-          )
-          }
-        </>
+      <div   key={arg} id={id} 
+       className="column-day">
+        {column = generateNumbers(0, 23).map(item =>(
+              <div key={item} id={id}   
+              data-time={item}
+              className="column-item">
+                {events.map(event => (
+                  <Event 
+                  key={Math.random()}
+                  events={events}
+                  id={event.id}
+                  title={event.title}
+                  description={event.description}
+                  startDate={event.startDate}
+                  startTime={event.startTime}
+                  />
+                )) }
+                {/* {events.map(event => id ===(event.date) &&(
+                 <Event
+                 key={event.id}
+                 id={event.id}
+               />
+               )
+                )}  */}
+              </div>
+        )
+        )
+        }
+
       </div>
     );
   });
