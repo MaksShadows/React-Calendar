b4169afa3a8928020f5db2f3e658cc2f9ae7e5c5
MaksShadows <Lykanenko1995@gmail.com>
2020-09-22 13:25:54 +0300

    112

--- src/gateway/events.js
+++ src/gateway/events.js
@@ -1,24 +1,24 @@
 const events = [
   {
-    //   id: 1,
-    //   title: 'Go to the gym',
-    //   description: 'some text here',
-    //   dateFrom: new Date(2020, 8, 18, 10, 0),
-    //   dateTo: new Date(2020, 8, 15, 13, 0),
+       id: 1,
+       title: 'Go to the gym',
+       description: 'some text here',
+       dateFrom: new Date(2020, 8, 21, 10, 0),
+       dateTo: new Date(2020, 8, 21, 13, 0),
   },
   {
       id: 2,
       title: 'Go to the school',
       description: 'hello, 2 am',
-      dateFrom: new Date(2020, 8, 16, 10, 0),
-      dateTo: new Date(2020, 8, 16, 11, 0),
+      dateFrom: new Date(2020, 8, 23, 10, 0),
+      dateTo: new Date(2020, 8, 23, 11, 0),
   },
   {
       id: 3,
       title: 'Lunch',
       description: '',
-      dateFrom: new Date(2020, 8, 17, 10, 30),
-      dateTo: new Date(2020, 8, 17, 11, 30),
+      dateFrom: new Date(2020, 8, 25, 10, 30),
+      dateTo: new Date(2020, 8, 25, 11, 30),
   },
 
 ]
